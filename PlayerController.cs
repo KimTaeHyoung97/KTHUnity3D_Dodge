302664c948515a5f597b7177575b09440f9dfe81
KimTaeHyoung97 <69450119+KimTaeHyoung97@users.noreply.github.com>
2020-08-17 18:05:29 +0900

    Update PlayerController.cs

--- PlayerController.cs
+++ PlayerController.cs
@@ -6,6 +6,14 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody rb;
     public float speed = 8f;
+    // Start is called before the first frame updateusing System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float speed = 8f;
     // Start is called before the first frame update
     void Start()
     {
@@ -29,5 +37,36 @@ public class PlayerController : MonoBehaviour
     public void Die()
     {
         gameObject.SetActive(false);
+
+        GameManager gm = FindObjectOfType<GameManager>();
+        gm.EndGame();
+    }
+}
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //키보드입력 = 스위치플랫폼 어느상황에서도 사용
+        float xInput = Input.GetAxis("Horizontal");//
+        float zInput = Input.GetAxis("Vertical");
+
+
+        float xSpeed = speed * xInput;
+        float zSpeed = speed * zInput;
+
+        rb.velocity = new Vector3(xSpeed, 0, zSpeed);
+    }
+
+    public void Die()
+    {
+        gameObject.SetActive(false);
+        
+        GameManager gm = FindObjectOfType<GameManager>();
+        gm.EndGame();
     }
 }
